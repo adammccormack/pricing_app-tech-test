0b66660f92fd468636dd0d084dacc99c7b63b68c
Adam <adam.j.mccor@gmail.com>
2022-04-27 21:15:18 +0100

    fixed issue with days conditional statement

--- lib/kitt_app.rb
+++ lib/kitt_app.rb
@@ -1,7 +1,6 @@
+# frozen_string_literal: true
 
 class KittApp
-  attr_accessor :week_price, :day_price, :days_remaining
-
   def get_price(minutes)
 
     # week price
@@ -14,10 +13,8 @@ class KittApp
 
       @days_remaining = @week_remainder*7
       @days_remaining = @days_remaining.to_i
-    @week_price
-  #end
-  #def get_day_price
-    #@days_remaining = @week_remainder*7
+      @week_price
+
       if @days_remaining >= 2 && @days_remaining <= 5
         @day_price = 105
       else
@@ -30,7 +27,7 @@ class KittApp
 
       elsif minutes < 10080 && minutes >= 1440
         days = minutes / 1440.to_f
-          if days >= 1.75 && days <= 5
+          if days >= 1.75 && days <= 7
             day_price = 105
           else
             day_price = days*60
@@ -64,25 +61,23 @@ class KittApp
   end
 end
 
-  # 3 days = 4320 minutes
+# 3 days = 4320 minutes
 
 # (1 week) + (2 days) = 12960 minutes = £210
 
+# use cheapest rate for duration
+# if duration is 3 hours (£66), then choose rate of £60 instead.
 
+# so if the number of days and hours meets and exceeds the cost of £105
+# then just make the cost £105
 
-  # use cheapest rate for duration
-  # if duration is 3 hours (£66), then choose rate of £60 instead.
+# price per (1 min)minute: £2
+#  price per (60 minutes)hour: £22
+# price per (1440 minutes)day: £60
+# price per (10080 minutes)week: £105
 
-  # so if the number of days and hours meets and exceeds the cost of £105
-  # then just make the cost £105
+# assuming 1 week is 5 business days
 
-  # price per (1 min)minute: £2
-  # price per (60 minutes)hour: £22
-  # price per (1440 minutes)day: £60
-  # price per (10080 minutes)week: £105
-
-  # assuming 1 week is 5 business days
-
-  # 1440 minutes = £60
-  # 2880 minutes = £105
-  # 20160 minutes = £210
+# 1440 minutes = £60
+# 2880 minutes = £105
+# 20160 minutes = £210
--- spec/kitt_app_spec.rb
+++ spec/kitt_app_spec.rb
@@ -1,55 +1,43 @@
+# frozen_string_literal: true
+
 require 'kitt_app'
 
 describe KittApp do
   let(:kitt_app) { KittApp.new }
-  
-    it { is_expected.to respond_to(:get_price).with(1).argument }
 
-    # when I pass an argument of 1 to get_price(), I will expect an output of 2. 
+  it { is_expected.to respond_to(:get_price).with(1).argument }
 
-    describe '#get_price' do
+  # when I pass an argument of 1 to get_price(), I will expect an output of 2.
 
-      it 'calculates the cheapest rate when given a duration in minutes' do
-        kitt_app
-
-        
-        expect(kitt_app.get_price(5)).to eq(10)
-        expect(kitt_app.get_price(11)).to eq(22)
-        expect(kitt_app.get_price(60)).to eq(22)
-        expect(kitt_app.get_price(120)).to eq(44)
-        expect(kitt_app.get_price(180)).to eq(60)
-        expect(kitt_app.get_price(1440)).to eq(60)
-        expect(kitt_app.get_price(2880)).to eq(105)
-        expect(kitt_app.get_price(20160)).to eq(210)
-      end
-
+  describe '#get_price' do
+    it 'calculates the cheapest rate when given a duration in minutes' do
+      expect(kitt_app.get_price(5)).to eq(10)
+      expect(kitt_app.get_price(11)).to eq(22)
+      expect(kitt_app.get_price(60)).to eq(22)
+      expect(kitt_app.get_price(120)).to eq(44)
+      expect(kitt_app.get_price(180)).to eq(60)
+      expect(kitt_app.get_price(1440)).to eq(60)
+      expect(kitt_app.get_price(2880)).to eq(105)
+      expect(kitt_app.get_price(20_160)).to eq(210)
     end
-
+  end
 end
-
 
 # price list
 
-# Price per minute: £2 
-# Price per hour: £22 
-# Price per day: £60 
+# Price per minute: £2
+# Price per hour: £22
+# Price per day: £60
 # Price per week: £105
-
 
 # Inputs
 
 # duration of booking in minutes
 
-
-
-
-
 # Outputs
 
 # cheapest price against The pricing function should use the cheapest rate for given duration.
 # For example if the duration is 3 hours it should use the daily price of £60 as this is cheaper than using the price per hour (£66).
-
-
 
 # edge cases
 
